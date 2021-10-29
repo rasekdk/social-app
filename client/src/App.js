2f28196ce8d4a7ed7097d2cde7793d938c598c3a
César Nóvoa <themotokar@gmail.com>
2021-10-29 20:37:47 +0200

    Created Client Side

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+
+import "./App.css";
+
+function App() {
+  return <div className="App">Hello world</div>;
+}
+
+export default App;
