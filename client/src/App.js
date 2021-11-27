ed7fe1f9a2580279a16eb27462027e20cc993a30
rasekdk <themotokar@gmail.com>
2021-11-27 19:48:49 +0100

    clear App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import Axios from "axios";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Home from "./components/Home";
