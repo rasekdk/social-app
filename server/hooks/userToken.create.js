c992519983ab95c9defbb983ec588e19f188a1d3
rasekdk <themotokar@gmail.com>
2021-11-27 19:44:23 +0100

    modified jwt

--- server/hooks/userToken.create.js
+++ server/hooks/userToken.create.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const createUserToken = (userData, expires = "30d") => {
   const tokenPayload = {
     id: userData._id,
-    name: userData.name,
+    user: userData.name,
   };
 
   const token = jwt.sign(tokenPayload, process.env.JWT_SECRET, {
