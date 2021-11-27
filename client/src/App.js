0462ff02916b15fac96b94efa426ba015a383010
rasekdk <themotokar@gmail.com>
2021-11-27 16:37:18 +0100

    Client Hello World

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,35 @@
 import React, { useState, useEffect } from "react";
 import Axios from "axios";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Home from "./components/Home";
+import NotFound from "./components/NotFound";
 
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  const { REACT_APP_API_URL } = process.env;
+
+  const [user, setUser] = useState([]);
+
+  useEffect(
+    () =>
+      Axios.get(`${REACT_APP_API_URL}/users`).then((res) => setUser(res.data)),
+    []
+  );
+
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="*">
+          <NotFound />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
