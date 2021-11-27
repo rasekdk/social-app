b53a2a9d015347aaddab13e290712c6ae2fc3476
rasekdk <themotokar@gmail.com>
2021-11-27 16:38:38 +0100

    Add ligon and get all users

--- server/controllers/userControllers/user.controllers.js
+++ server/controllers/userControllers/user.controllers.js
@@ -1,5 +1,9 @@
 const register = require("./user.register");
+const login = require("./user.login");
+const getUsers = require("./user.getUsers");
 
 module.exports = {
   register,
+  login,
+  getUsers,
 };
